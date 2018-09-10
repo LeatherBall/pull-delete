a493159b5c434efe70faa996e51b666081dc2609
Leather Ball <43058637+LeatherBall@users.noreply.github.com>
2018-09-10 13:10:21 +0800

    update pulldelete.js

--- res/pulldelete.js
+++ res/pulldelete.js
@@ -3,9 +3,15 @@
  * 左滑删除
  */
 (function(win, $) {
-    var startX, startY, pullX, pullY, moveX, moveY,
-        rem = parseFloat($('html').css('font-size').replace('px', '')),
-        btn = 2.5 * rem, threshold = btn / 2;
+    var startX, // 起始点X
+        startY, // 起始点Y
+        pullX,  // 结束点X
+        pullY,  // 结束点Y
+        moveX,  // X轴拖动距离
+        moveY,  // Y轴拖动距离
+        rem,    // css单位
+        btn,    // 删除按钮宽度
+        threshold; // 拖动阈值
     var _this;
 
     /**
@@ -16,6 +22,9 @@
      */
     var PullDelete = function ($Dom, callback) {
         _this = this;
+        rem = parseFloat($('html').css('font-size').replace('px', '')),
+        btn = 2.5 * rem;
+        threshold = btn / 2;
         if ($Dom instanceof jQuery && $Dom.hasClass('pull_delete')) {
             $Dom.append('<i class="pd_btn"></i>');
             _this.addPullListener($Dom, callback);
@@ -39,8 +48,6 @@
                 moveY = (startY - pullY) * 0.8;
                 // 阻止纵向推拽
                 if (Math.abs(moveY) > Math.abs(moveX)) {
-                    e.stopPropagation();
-                    e.preventDefault();
                     return;
                 }
                 if (moveX <= btn) {
@@ -53,6 +60,8 @@
                 $(this).addClass('trans').css({transform: 'translateX(-' + viewTrans + 'px)'});
                 // 点击删除
                 if (e.target.className == 'pd_btn') {
+                    e.stopPropagation();
+                    e.preventDefault();
                     if (callback) {
                         callback($Dom);
                     }
